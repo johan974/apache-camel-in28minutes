d8c6ff9cb270fc69c69ef2db6288ba7c699da9b0
johan974 <jj974@upcmail.nl>
2022-11-18 20:44:27 +0100

    Some few additions while playing around

--- 02.projects/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ 02.projects/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,7 +1,5 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.a;
 
-import java.time.LocalDateTime;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -10,70 +8,82 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class MyFirstTimerRouter extends RouteBuilder{
-	
-	@Autowired
-	private GetCurrentTimeBean getCurrentTimeBean; 
-	
-	@Autowired
-	private SimpleLoggingProcessingComponent loggingComponent;
+import java.time.LocalDateTime;
 
-	@Override
-	public void configure() throws Exception {
-		// timer
-		// transformation
-		// log
-		// Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
-		from("timer:first-timer") //null
-		.log("${body}")//null
-		.transform().constant("My Constant Message")
-		.log("${body}")//My Constant Message
-		//.transform().constant("Time now is" + LocalDateTime.now())
-		//.bean("getCurrentTimeBean")
+@Component
+public class MyFirstTimerRouter extends RouteBuilder {
 
-		//Processing
-		//Transformation
-		
-		.bean(getCurrentTimeBean)
-		.log("${body}")//Time now is2021-01-18T18:32:19.660244
-		.bean(loggingComponent)
-		.log("${body}")
-		.process(new SimpleLoggingProcessor())
-		.to("log:first-timer"); //database
-		
+  @Autowired
+  private GetCurrentTimeBean getCurrentTimeBean;
 
-	}
+  @Autowired
+  private SimpleLoggingProcessingComponent loggingComponent;
+
+  @Autowired
+  private DuplicateMessage duplicateMessage;
+
+  @Override
+  public void configure() throws Exception {
+    // timer
+    // transformation
+    // log
+    // Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
+    from("timer:first-timer") //null
+            .log("${body}")//null
+            .transform().constant("My Constant Message")
+            .log("${body}")//My Constant Message
+            //.transform().constant("Time now is" + LocalDateTime.now())
+            //.bean("getCurrentTimeBean")
+
+            //Processing
+            //Transformation
+
+            .bean(getCurrentTimeBean)
+            .log("${body}")//Time now is2021-01-18T18:32:19.660244
+            .bean(loggingComponent) // heeft een void method - leave body as it is
+            .log("${body}")
+            .bean(duplicateMessage)
+            .log("${body}")
+            .process(new SimpleLoggingProcessor())
+            .to("log:first-timer"); //database
+  }
 
 }
 
 @Component
 class GetCurrentTimeBean {
-	public String getCurrentTime() {
-		return "Time now is" + LocalDateTime.now();
-	}
+  public String getCurrentTime() {
+    return "Time now is" + LocalDateTime.now();
+  }
 }
 
 @Component
 class SimpleLoggingProcessingComponent {
-	
-	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
 
-	public void process(String message) {
-		
-		logger.info("SimpleLoggingProcessingComponent {}", message);
-		
-	}
+  private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+
+  public void process(String message) {
+
+    logger.info("SimpleLoggingProcessingComponent {}", message);
+
+  }
+}
+
+@Component
+class DuplicateMessage {
+  public String process(String message) {
+    return "Duplicated: " + message + "+" + message;
+  }
 }
 
 
 class SimpleLoggingProcessor implements Processor {
-	
-	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
 
-	@Override
-	public void process(Exchange exchange) throws Exception {
-		logger.info("SimpleLoggingProcessor {}", exchange.getMessage().getBody());
-	}
+  private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+
+  @Override
+  public void process(Exchange exchange) throws Exception {
+    logger.info("SimpleLoggingProcessor {}", exchange.getMessage().getBody());
+  }
 
 }
